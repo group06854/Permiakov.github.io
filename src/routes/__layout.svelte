1ccd5a9ac7cd485847e402c5e1c55835ad77f070
Lev Permiakov <l.permiakov@innopolis.university>
2024-07-06 02:26:22 +0400

    go to another rep

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+
+    const changeColor = (event : MouseEvent) => {
+        const inf = document.getElementById("Homework");
+        if (inf){
+        inf.style.backgroundColor = 'grey';
+        }
+        
+    }
+    
+    </script>
+    <header class = "container">
+        <span class = 'logo'>Lev Permiakov</span>
+        <nav>
+            <ul>
+                <li><a id = "nav" href="#head_st">About Me</a></li>
+                <li><a id = "nav" href="#head">Portfolio</a></li>
+                <li><a id = "nav" href="#head_end">Contact Me</a></li>
+                <li><a id = "nav" href="/Comic">Homework</a></li>
+            </ul>
+        </nav>
+    </header>
+    <style>
+    
+    a{
+        text-decoration: none;
+        color:aliceblue;
+    }
+    .container{
+        margin-top: 1px;
+        width: 98%;
+        margin-left: 0.5%;
+        margin-right: 0.5%;
+    }
+    header{
+        padding: 10px;
+        margin-bottom: 40px;
+    }
+    header .logo{
+        width: 10%;
+        font-weight: 800;
+        text-transform: uppercase; 
+        color: white;
+    }
+    header ul{
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+    }
+    
+    header nav{
+        float: right;
+        width: 50%;
+    }
+    header nav ul{
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+    }
+    header ul li{
+        display: inline-block;
+    }
+    #nav{
+        color: white;
+    }
+    
+    </style>
